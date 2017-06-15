016a5d374d41ce55724375628cc1e593311771f2
Rhys Devine-Davies <d019804a@student.staffs.ac.uk>
2017-06-15 14:59:33 +0100

    moved index.tsx

---
+++ index.tsx
@@ -0,0 +1,7 @@
+import { NgController } from "./src/navgroups/ngcontroller";
+import { NavItem      } from "./src/navgroups/navitem";
+import { NavGroup     } from "./src/navgroups/navgroup";
+
+export {
+    NgController, NavItem, NavGroup
+}
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NgController } from "./navgroups/ngcontroller";
-import { NavItem      } from "./navgroups/navitem";
-import { NavGroup     } from "./navgroups/navgroup";
-
-export {
-    NgController, NavItem, NavGroup
-}
